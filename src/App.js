937c3265203c165941f553484c614d2a2e2af6ef
stephanygrace <stephanytayong73@gmail.com>
2023-03-18 19:54:48 +0800

    Pops vs State

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 // import Hello from './components/Hello'
+import Message from './components/Message'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <Greet name="Doms" heroName="Superman">
+        {/* <Greet name="Doms" heroName="Superman">
           This is the Children props
         </Greet>
         <Greet name="Tin" heroName="Batman"/>
@@ -16,7 +17,9 @@ class App extends Component {
         <Greet name="Domy" heroName="My Hero"/>
 
         <Welcome name="Doms" heroName="Superman" />
-        <Welcome name="Domsyy" heroName="Supermanny" />
+        <Welcome name="Domsyy" heroName="Supermanny" /> */}
+
+        <Message />
       </div>
     )
   }
